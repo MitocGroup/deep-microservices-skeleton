11b741d29d4253c039ad015bd03978729c9c363e
delned <rudicov@gmail.com>
2016-04-08 16:14:55 +0300

    Test a solution for PhantomJS crashes. Test on multiple nodeJS versions.

--- bin/test/phantomjs/install.sh
+++ bin/test/phantomjs/install.sh
@@ -6,13 +6,13 @@
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
 if [[ ${TRAVIS_NODE_VERSION} == 5.* ]]; then
-   echo "my version: ${TRAVIS_NODE_VERSION}"
+   echo "My version: ${TRAVIS_NODE_VERSION}"
 
     npm install -g phantomjs@2.1.3
 else
-    echo "my version: ${TRAVIS_NODE_VERSION}"
+    echo "My version: ${TRAVIS_NODE_VERSION}"
     #fix for issue: https://github.com/Medium/phantomjs/issues/430#issuecomment-174038299
-    npm config set unsafe-perm true
+    npm config set unsafe-perm false
     npm install -g phantomjs@1.9.18
     npm config set unsafe-perm true
 fi
