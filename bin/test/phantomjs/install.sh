7cc14b81578a046b943c145b172caba040a0538c
delned <rudicov@gmail.com>
2016-04-12 16:47:05 +0300

    Fix generic paths for protractor/phantomjs

--- bin/test/phantomjs/install.sh
+++ bin/test/phantomjs/install.sh
@@ -2,7 +2,8 @@
 #
 # Created by vcernomschi on 10/06/2015
 #
-source $(dirname $0)/_head.sh
+source $(dirname $0)/../_head.sh
+
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
 # Fix for issue: https://github.com/Medium/phantomjs/issues/430#issuecomment-174038299
