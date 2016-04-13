acd512758a0b957a77ff40538d702243a04a4a66
Serghei Rudicov <srudicov@pycoding.biz>
2016-04-13 18:23:06 +0300

    #3 Remove deps from package.json
    * Copy package.json in root with travis
    
    * Test a solution for PhantomJS crashes. Test on multiple nodeJS versions
    
    * Test a solution for PhantomJS crashes. Test on multiple nodeJS versions.
    
    * Test a solution for PhantomJS crashes. Run a new build
    
    * Update .travis.yml
    
    * Set new enviroment var for E2E.
    
    * Set new enviroment var for E2E tests.
    
    * Skip e2e test
    
    * Move dependencies from travis.yml to package.json
    
    * Refactor scripts for E2E tests
    
    * Fix scripts for E2E tests
    
    * Fix scripts for E2E tests
    
    * Fix generic paths for protractor/phantomjs
    
    * Set timers to prepare step
    
    * Test sauce connection from prepare
    
    * Call sauce connection from prepare
    
    * Install sauce-connection from prepare
    
    * Update .travis.yml

--- bin/test/phantomjs/install.sh
+++ bin/test/phantomjs/install.sh
@@ -3,10 +3,19 @@
 # Created by vcernomschi on 10/06/2015
 #
 
+#source $(dirname $0)/../_head.sh
+
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
-if [[ ${TRAVIS_NODE_VERSION} == 5.* ]]; then
-  npm install -g phantomjs@2.1.3
-else
-  npm install -g phantomjs@1.9.18
-fi
+# Fix for issue: https://github.com/Medium/phantomjs/issues/430#issuecomment-174038299
+npm config set unsafe-perm false
+
+#if [ "${__TRAVIS_NODE_MAJOR_VERSION}" = "5" ]; then
+#  echo "My version: ${TRAVIS_NODE_VERSION}"
+#  npm install -g phantomjs@2.1.3
+#else
+#  echo "My version: ${TRAVIS_NODE_VERSION}"
+
+npm install -g phantomjs@1.9.18
+
+npm config set unsafe-perm true
