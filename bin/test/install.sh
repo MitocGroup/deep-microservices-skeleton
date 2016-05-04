02ea2d1e203a28588c7caba82ef33ff3f71f83ef
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-04 15:44:41 +0300

    Add paralize installing deps

--- bin/test/install.sh
+++ bin/test/install.sh
@@ -9,4 +9,11 @@ source $(dirname $0)/_head.sh
 ### Install NPM deps ###
 ########################
 __CMD="npm install"
-subpath_run_cmd "${__SRC_PATH}" "$__CMD"
+
+subpath_run_cmd "${__SRC_PATH}" "$__CMD" "$__CMD" ${0}
+
+if [ -z "${0}" ] && [ "${0}"="backend" ]; then
+  echo "Initializing backend"
+  cp src/deeploy.example.json src/deeploy.json &&\
+  deepify init-backend ./src
+fi
