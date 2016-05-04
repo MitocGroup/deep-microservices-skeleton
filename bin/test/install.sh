fe44b90d22c9ffce0c9863d84b58d5399bc678af
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-04 16:44:57 +0300

    Update install.sh to install frontend deps when e2e enabled

--- bin/test/install.sh
+++ bin/test/install.sh
@@ -19,7 +19,11 @@ if [ -z "${0}" ] && [ "${0}"="backend" ]; then
   # To disable interactive user interaction like prompts in terminal (an default value is always chosen)
   export DEEP_NO_INTERACTION=1
 
-  subpath_run_cmd "${__SRC_PATH}" "$__CMD" "$__CMD" "frontend"
+  #install also front for repos with e2e enabled
+  if [ "${__TRAVIS_NODE_MAJOR_VERSION}" != "5" ] && [ "${__TRAVIS_NODE_MAJOR_VERSION}" != "6" ] && \
+   ([ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ] || ([ "${__E2E_WITH_PRIVATE_REPO}" = "${E2E_TESTING}" ] && [ ${TRAVIS_BRANCH} = 'stage' ])); then
+    subpath_run_cmd "${__SRC_PATH}" "$__CMD" "$__CMD" "frontend"
+  fi
 
   echo "Initializing backend"
   cp src/deeploy.example.json src/deeploy.json &&\
